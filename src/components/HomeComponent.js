74d419cf1a6b241f645537eefde74adb0329e3c4
Yasir Affan <yasiraffan9@gmail.com>
2021-01-14 15:28:42 +0530

    removed unnecessary variables

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
 		<div>
 			<h2>Select the project</h2>
 			<ul class="list-group">
-				<li class="list-group-item disabled">
+				{/* <li class="list-group-item disabled">
 					<Link to="/">Home </Link>
-				</li>
+				</li> */}
 				<li class="list-group-item">
 					<Link to="/randomQuoteGenerator">
 						Random Quote Generator
